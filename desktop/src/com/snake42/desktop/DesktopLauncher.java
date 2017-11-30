adf996a48e7c84f9f421d146ce8e584affc81b27
Kawaisaki <satoaki.se@gmail.com>
2017-11-30 21:34:48 +0100

    first try

---
+++ desktop/src/com/snake42/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.snake42.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.snake42.Snake42;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Snake42(), config);
+	}
+}
---
+++ html/src/com/snake42/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.snake42.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.snake42.Snake42;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Snake42();
+        }
+}
